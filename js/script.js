87a719a52ab9d5d1eda421a49e79379b56cdc9b1
Emmanuela Joy <emmajoy81@gmail.com>
2021-02-27 15:33:27 +0300

    add a mini calculator

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
-window.addEventListener("DOMContentLoaded", function() {
-console.log("Content Loaded");
+
+var add = function (number1, number2) {
+    return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number:"))
+var result = add(number1, number2);
+alert("The addition of your numbers is: " + result);
+
+
+// Subtraction
+var subtract = (number1, number2) => number1 - number2;
+var result = subtract(number1, number2);
+    alert("The difference between your numbers is " + result);
+
+// Multipliction
+var multiply = (number1, number2) => number1 * number2;
+var result = multiply(number1, number2);
+    alert("The multiplication of your numbers is " + result);
+
+// Division
+var divide = (number1, number2) => number1 / number2;
+var result = divide(number1, number2);
+alert("The division of your numbers is " + result); 
+
+
+    /*var multiply = (number1, number2, number3) => number1 * number2 * number3;
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter a second number:"));
+    var number3 = parseInt(prompt("Enter a third number:"));
+    var result = multiply(number1, number2, number3);
+    alert("The result is " + result);
+    
+    var divide = (number1, number2) => number1 / number2
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter a second number:"));
+    var result = divide(number1, number2);
+    alert("The result is " + result); */
 
 
 
@@ -34,7 +71,3 @@ console.log("Content Loaded");
 
 
 
-
-
-
-})
